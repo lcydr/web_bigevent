39d2234ae521afb13546ec5846149ee6a7ea0c62
lcydr <1211753823@qq.com>
2022-06-11 09:50:07 +0800

    完成index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,16 @@
-$.ajaxPrefilter((options) =>{
-    options.url = "http://www.liulongbin.top:3007" + options.url
-})
+$.ajaxPrefilter((options) => {
+    options.url = "http://www.liulongbin.top:3007" + options.url;
+    if (options.url.indexOf("/my/")) {
+        options.headers = {
+            Authorization: localStorage.getItem("token"),
+        };
+    }
+    // 权限校验
+    options.complete = (res) => {
+        console.log(res)
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = "/login.html"
+        }
+    };
+});
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: "/my/userinfo",
+        // headers: {
+        //     Authorization: localStorage.getItem("token"),
+        // },
+        success: (res) => {
+            if (res.status !== 0) return layer.msg(res.message);
+            layer.msg("获取用户信息成功")
+            renderAvatar(res.data)
+        },
+    });
+}
+const renderAvatar = (user) => {
+    const name = user.nickname || user.username;
+
+    $("#welcome").html(`欢迎 ${name}`)
+
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr("src", user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        let first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
+}
+
+// 退出
+$("#btnLogout").click(() => {
+    layer.confirm("是否退出登录", { icon: 3, title: "提示" }, function (index) {
+        localStorage.removeItem("token");
+        location.href = "/login.html";
+    })
+})
+
+
+
+
+getUserInfo()
